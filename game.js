511f71f041f013eb47a087c2b609ea212386225c
Burak Karamete <32237447+bkmete99@users.noreply.github.com>
2020-01-24 18:33:37 +0300

    Update game.js

--- game.js
+++ game.js
@@ -9,8 +9,8 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = %100;
-        this.canvas.height = %100;
+        this.canvas.width = 1280;
+        this.canvas.height = 720;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
